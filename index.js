f72614219d058142ad38f72ef62c8bedd4271674
andyzheng679 <andyzheng679@gmail.com>
2024-04-13 14:33:30 -0400

    formating

---
+++ index.js
@@ -0,0 +1,36 @@
+// Import stylesheets
+//import './style.css';
+var form = document.querySelector('#defineform');
+form.onsubmit = function () {
+    var formData = new FormData(form);
+    console.log(formData);
+    var text = formData.get('defineword');
+    console.log(text);
+    fetch("https://api.dictionaryapi.dev/api/v2/entries/en/".concat(text))
+        .then(function (response) {
+        return response.json();
+    })
+        .then(function (data) {
+        appendData(data);
+    })
+        .catch(function (err) {
+        console.log('error: ' + err);
+    });
+    return false; // prevent reload
+};
+function appendData(data) {
+    var mainContainer = document.getElementById("quotes");
+    // let p = document.createElement("p");
+    // let w = document.createElement("p");
+    var word = document.getElementById("word");
+    var definition = document.getElementById("definition");
+    var definitiontwo = document.getElementById("definitiontwo");
+    var definitionthree = document.getElementById("definitionthree");
+    word.innerHTML = data[0].word;
+    definition.innerHTML = data[0].meanings[0].partOfSpeech + ": " + data[0].meanings[0].definitions[0].definition;
+    definitiontwo.innerHTML = data[0].meanings[1].partOfSpeech + ": " + data[0].meanings[1].definitions[0].definition;
+    definitionthree.innerHTML = data[0].meanings[2].partOfSpeech + ": " + data[0].meanings[2].definitions[0].definition;
+    console.log(data[0]);
+    // mainContainer.appendChild(w);
+    // mainContainer.appendChild(p);
+}
--- index.ts
+++ index.ts
@@ -1,9 +1,8 @@
 // Import stylesheets
-import './style.css';
+//import './style.css';
 
 
-const form: HTMLFormElement = document.querySelector('#defineform');
-
+const form: HTMLFormElement = document.querySelector('#defineform')!;
 
 form.onsubmit = () => {
   const formData = new FormData(form);
@@ -11,5 +10,36 @@ form.onsubmit = () => {
   console.log(formData);
   const text = formData.get('defineword') as string;
   console.log(text);
+
+  fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${text}`)
+.then(function (response) {
+    return response.json();
+})
+.then(function(data) {
+    appendData(data);
+})
+.catch(function(err) {
+    console.log('error: ' + err);
+})
+
   return false; // prevent reload
+
 };
+
+function appendData(data) {
+  let mainContainer = document.getElementById("quotes")!;
+  // let p = document.createElement("p");
+  // let w = document.createElement("p");
+  let word = document.getElementById("word")!;
+  let definition = document.getElementById("definition")!;
+  let definitiontwo = document.getElementById("definitiontwo")!;
+  let definitionthree = document.getElementById("definitionthree")!;
+
+  word.innerHTML = data[0].word;
+  definition.innerHTML = data[0].meanings[0].partOfSpeech + ": " + data[0].meanings[0].definitions[0].definition;
+  definitiontwo.innerHTML = data[0].meanings[1].partOfSpeech + ": " + data[0].meanings[1].definitions[0].definition;
+  definitionthree.innerHTML = data[0].meanings[2].partOfSpeech + ": " + data[0].meanings[2].definitions[0].definition;
+  console.log(data[0])
+  // mainContainer.appendChild(w);
+  // mainContainer.appendChild(p);
+}
