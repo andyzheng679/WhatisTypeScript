42b5882a87c0cfe98b173ddc492e4b8caf70dad6
andyzheng679 <andyzheng679@gmail.com>
2024-04-13 19:30:21 -0400

    adding looping

--- index.js
+++ index.js
@@ -19,19 +19,43 @@ form.onsubmit = function () {
     return false; // prevent reload
 };
 function appendData(data) {
-    var mainContainer = document.getElementById("quotes");
+    // let mainContainer = document.getElementById("quotes")!;
     // let p = document.createElement("p");
     // let w = document.createElement("p");
     var word = document.getElementById("word");
+    var phonetics = document.getElementById("phonetics");
     var definition = document.getElementById("definition");
-    var definitiontwo = document.getElementById("definitiontwo");
-    var definitionthree = document.getElementById("definitionthree");
-    var synonymsone = document.getElementById("synonymsone");
+    // let definitiontwo = document.getElementById("definitiontwo")!;
+    // let definitionthree = document.getElementById("definitionthree")!;
+    // let synonymsone = document.getElementById("synonymsone")!;
     word.innerHTML = data[0].word;
-    definition.innerHTML = data[0].meanings[0].partOfSpeech + ": " + data[0].meanings[0].definitions[0].definition;
-    definitiontwo.innerHTML = data[0].meanings[1].partOfSpeech + ": " + data[0].meanings[1].definitions[0].definition;
-    definitionthree.innerHTML = data[0].meanings[2].partOfSpeech + ": " + data[0].meanings[2].definitions[0].definition;
-    synonymsone.innerHTML = data[0].meanings[0].synonyms[0];
+    var phoneticFound = false;
+    for (var i = 0; i < data[0].phonetics.length; i++) {
+        if (data[0].phonetics[i].text) {
+            phonetics.innerHTML = data[0].phonetics[i].text;
+            phoneticFound = true;
+            break;
+        }
+    }
+    if (!phoneticFound) {
+        phonetics.innerHTML = "N/A";
+    }
+    var definitionFound = false;
+    for (var i = 0; i < data[0].meanings.length; i++) {
+        if (data[0].meanings[i].definitions[0].definition) {
+            definition.innerHTML = data[0].meanings[i].definitions[0].definition;
+            definitionFound = true;
+            break;
+        }
+    }
+    if (!definitionFound) {
+        definition.innerHTML = "N/A";
+    }
+    // phonetics.innerHTML = data[0].phonetics[1].text;
+    // definition.innerHTML = data[0].meanings[0].partOfSpeech + ": " + data[0].meanings[0].definitions[0].definition;
+    // definitiontwo.innerHTML = data[0].meanings[1].partOfSpeech + ": " + data[0].meanings[1].definitions[0].definition;
+    // definitionthree.innerHTML = data[0].meanings[2].partOfSpeech + ": " + data[0].meanings[2].definitions[0].definition;
+    // synonymsone.innerHTML = data[0].meanings[0].synonyms[0];
     console.log(data[0]);
     // mainContainer.appendChild(w);
     // mainContainer.appendChild(p);
--- index.ts
+++ index.ts
@@ -27,20 +27,51 @@ form.onsubmit = () => {
 };
 
 function appendData(data) {
-  let mainContainer = document.getElementById("quotes")!;
+  // let mainContainer = document.getElementById("quotes")!;
   // let p = document.createElement("p");
   // let w = document.createElement("p");
   let word = document.getElementById("word")!;
+  let phonetics = document.getElementById("phonetics")!;
   let definition = document.getElementById("definition")!;
-  let definitiontwo = document.getElementById("definitiontwo")!;
-  let definitionthree = document.getElementById("definitionthree")!;
-  let synonymsone = document.getElementById("synonymsone")!;
+  // let definitiontwo = document.getElementById("definitiontwo")!;
+  // let definitionthree = document.getElementById("definitionthree")!;
+  // let synonymsone = document.getElementById("synonymsone")!;
 
   word.innerHTML = data[0].word;
-  definition.innerHTML = data[0].meanings[0].partOfSpeech + ": " + data[0].meanings[0].definitions[0].definition;
-  definitiontwo.innerHTML = data[0].meanings[1].partOfSpeech + ": " + data[0].meanings[1].definitions[0].definition;
-  definitionthree.innerHTML = data[0].meanings[2].partOfSpeech + ": " + data[0].meanings[2].definitions[0].definition;
-  synonymsone.innerHTML = data[0].meanings[0].synonyms[0];
+  
+  
+  let phoneticFound = false;
+  
+  for (let i = 0; i < data[0].phonetics.length; i++) {
+  if (data[0].phonetics[i].text) {
+    phonetics.innerHTML = data[0].phonetics[i].text;
+    phoneticFound = true;
+    break;
+  }
+}
+if (!phoneticFound) {
+  phonetics.innerHTML = "N/A";
+}
+
+
+let definitionFound = false;
+
+for (let i = 0; i < data[0].meanings.length; i++){
+  if(data[0].meanings[i].definitions[0].definition){
+    definition.innerHTML = data[0].meanings[i].definitions[0].definition;
+    definitionFound = true;
+    break;
+  }
+}
+if (!definitionFound) {
+  definition.innerHTML = "N/A";
+}
+
+  // phonetics.innerHTML = data[0].phonetics[1].text;
+  // definition.innerHTML = data[0].meanings[0].partOfSpeech + ": " + data[0].meanings[0].definitions[0].definition;
+  // definitiontwo.innerHTML = data[0].meanings[1].partOfSpeech + ": " + data[0].meanings[1].definitions[0].definition;
+  // definitionthree.innerHTML = data[0].meanings[2].partOfSpeech + ": " + data[0].meanings[2].definitions[0].definition;
+  // synonymsone.innerHTML = data[0].meanings[0].synonyms[0];
 
   console.log(data[0])
   // mainContainer.appendChild(w);
